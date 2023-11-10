00da096f362d808a25a87cfeb9709e4057e01701
Nathan Caio <nathancaio1715@gmail.com>
2023-11-10 13:38:41 -0300

    feat(homepage): create About Me section

---
+++ src/pages/Home/components/AboutMe/index.tsx
@@ -0,0 +1,83 @@
+import { Container, Flex, Box, Heading, Text } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
+
+export function AboutMe(): JSX.Element {
+	return (
+		<Container
+			as='section'
+			maxW='6xl'
+			pt='5rem'
+		>
+			<Flex
+				direction='column'
+				gap='4rem'
+			>
+				<Box as='section'>
+					<Heading
+						as={motion.h2}
+						position={'relative'}
+						initial={{
+							opacity: 0,
+							top: '1rem',
+						}}
+						whileInView={{
+							opacity: 1,
+							top: 0,
+						}}
+					>
+						But hey, what's about me?
+					</Heading>
+					<Text
+						as={motion.p}
+						position='relative'
+						initial={{
+							opacity: 0,
+							left: '-1rem',
+						}}
+						whileInView={{
+							opacity: 1,
+							left: 0,
+						}}
+					>
+						My name is Nathan Caio, I am from Brazil and I am a web developer.
+						The coolest part, in my humble opinion, is that I am a pretty much
+						of a <Text as='em'>self-taught</Text> developer, so yes, I know how
+						to Google stuff.
+					</Text>
+				</Box>
+				<Box as='section'>
+					<Heading
+						as={motion.h2}
+						position='relative'
+						initial={{
+							opacity: 0,
+							top: '1rem',
+						}}
+						whileInView={{
+							opacity: 1,
+							top: 0,
+						}}
+					>
+						And what do I do?
+					</Heading>
+					<Text
+						as={motion.p}
+						position='relative'
+						initial={{
+							opacity: 0,
+							left: '-1rem',
+						}}
+						whileInView={{
+							opacity: 1,
+							left: 0,
+						}}
+					>
+						As I said, I am a web developer, but I have a focus on Front-End. My
+						main stacks that I use to build web applications are React and
+						Next.js. Say, doesn't that sound cool? Of course it does!
+					</Text>
+				</Box>
+			</Flex>
+		</Container>
+	);
+}
