e9f79aa886c51ae258dc45418599ca07620e078f
Nathan Caio <nathancaio1715@gmail.com>
2024-12-12 20:33:56 -0300

    feat(locale): create locale object

---
+++ locales/en-us.mjs
@@ -0,0 +1,102 @@
+const enUS = {
+  'nav-item-1': '[About]',
+  'nav-item-2': '[Experiences]',
+  'nav-item-3': '[Education]',
+  'nav-item-4': '[Skills]',
+  'nav-item-5': '[Languages]',
+  'nav-item-6': '[Contact]',
+  'language-indicator': 'Language: English',
+  'switch-language-btn': '[Mudar para português]',
+  'about-title': 'Hello, <em>world</em>!',
+  'about-text-1': `Hello, my name is Nathan Caio, I'm a Front-End developer specialized in
+		<a
+			href="https://react.dev/"
+			target="_blank"
+			>React</a
+		>
+		and
+		<a
+			href="https://nextjs.org/"
+			target="_blank"
+			>Next.js</a
+		>
+		with 4 years of professional experience. As a <em>self-taught</em> person, I developed skills
+		throughout my career such as proactivity and constant focus on learning in order to
+		maintain continuous growth.`,
+  'about-text-2': `My trajectory has been marked by experiences such as working as a <em>focal developer</em>,
+		collaborating with the technical leadership and the business team to work on high <em>quality solutions</em>.`,
+  'about-text-3': `In one of the projects I've worked on, I've <em>led the Front-End</em>, being responsible
+		for the main technical decisions, as well as documenting components, concepts, flows and processes to ensure
+		clarity and efficiency. I've always been dedicated to supporting and mentoring less experienced developers,
+		promoting an environment of learning and collaboration.`,
+  'about-text-4': `My main technical skills are <em>HTML</em>, <em>CSS</em>, <em>JavaScript</em>, <em>TypeScript</em>,
+		<em>React</em>, <em>Next.js</em>, <em>Jest</em> or <em>Vitest</em>, code standards and <em>SOLID</em> principles,
+		among others.`,
+  'about-text-5': `My goal is to focus on keep evolving as a developer, always looking for improving my skills
+		and contribute in challenging projects that have a positive impact both on the market as a whole and on the
+		life and experience of the users.`,
+  'experiences-title': 'Experiences',
+  'experiences-subtitle': 'Some of my professional experiences.',
+  'experiences-1-date': 'Dec/2023 - Present',
+  'experiences-1-role': 'Front-End React Developer',
+  'experiences-1-list': `
+		<li>Work on projects such as <em>Cresol</em> and <em>Vivo - Vale Saúde</em>.</li>
+		<li>Maintenance and development of new functionalities.</li>
+		<li>Creation of documentation of components, concepts and flows.</li>
+		<li>Performance as focal developer in conjunction with technical and business leadership.</li>
+	`,
+  'experiences-2-date': 'Jan/2022 - Jun/2023',
+  'experiences-2-role': 'Front-End React Developer',
+  'experiences-2-list': `
+		<li>Work on e-commerce projects such as <em>KitKat Chocolatory Brasil</em> and <em>Hortifruti
+			Natural da Terra</em>.</li>
+		<li>Front-End leadership on the <em>KitKat Chocolatory</em> project, responsible for the decision
+			taking and well being of the application.</li>
+		<li>Responsible for implementing new code quality standards, unit testsm documentation of components,
+			flows and processes.</li>
+	`,
+  'experiences-3-date': 'Jun/2021 - Jan/2022',
+  'experiences-3-role': 'Full-Stack Developer',
+  'experiences-3-list': `
+		<li>Work on the company's internal dashboard project, being responsible for the complete development of
+			both Front-End and Back-End application.</li>
+		<li>Implementation of several integrations, such as graphs, maps, user control and suchlike.</li>
+	`,
+  'education-title': 'Education',
+  'education-subtitle': 'School trajectory, courses and other studies',
+  'education-1-title': '[Exchange in Dublin, Ireland]',
+  'education-1-date': '3 weeks - Aug/2023',
+  'education-2-title': '[Complete High School]',
+  'education-2-date': 'Dec/2018',
+  'education-complementary-title': 'Complementary education',
+  'education-complementary-1-title': '[Computer Science Curriculum]',
+  'education-complementary-1-date': 'Present',
+  'skills-title': 'Skills',
+  'skills-subtitle': 'Points that worth citing.',
+  'skills-item-1': '[Self-taught formation]',
+  'skills-item-2': '[Proactivity]',
+  'skills-item-3': '[Support and metoring for less experienced developers]',
+  'skills-item-4': '[Growth through continuous study]',
+  'skills-item-5': '[Design Systems]',
+  'skills-item-6': '[State Management with Redux or similar libraries]',
+  'skills-item-7': '[Code patterns and SOLID principles]',
+  'skills-item-8': '[Documentation of components and flows]',
+  'skills-item-9': '[Unit test with Jest and Vitest]',
+  'skills-item-10': '[Storybook]',
+  'skills-item-11': '[React]',
+  'skills-item-12': '[Next.js]',
+  'skills-item-13': '[JavaScript]',
+  'skills-item-14': '[TypeScript]',
+  'skills-item-15': '[HTML]',
+  'skills-item-16': '[CSS]',
+  'languages-title': 'Languages',
+  'languages-subtitle': 'Languages that I have knowledge on.',
+  'languages-1-name': '[Portuguese]',
+  'languages-1-level': 'Level of knowledge: <em>Native</em>',
+  'languages-2-name': '[English]',
+  'languages-2-level': 'Level of knowledge: <em>Advanced</em>',
+  'contact-title': 'Contact',
+  'contact-subtitle': 'Ways to enter in contact with me.',
+};
+
+export default enUS;
