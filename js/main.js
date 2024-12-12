db4388fdf1866cf5991bbcb59cfd1c6a30e51c44
Nathan Caio <nathancaio1715@gmail.com>
2024-12-12 20:33:13 -0300

    feat(locale): create locale logic

---
+++ js/main.js
@@ -0,0 +1,28 @@
+import en from '../locales/en-us.mjs';
+
+const { localStorage } = window;
+const lang = localStorage.getItem('nathancaio_portfolio@language');
+
+const switchLangBtn = document.querySelector(
+  'button[name=switch-language-btn]'
+);
+
+switchLangBtn.addEventListener('click', () => {
+  localStorage.setItem(
+    'nathancaio_portfolio@language',
+    lang === 'en-us' ? 'pt-br' : 'en-us'
+  );
+  window.location.reload();
+});
+
+if (lang === 'en-us') {
+  const itens = document.querySelectorAll('[data-locale]');
+
+  for (let i = 0; i < itens.length; i++) {
+    const itemLocale = en[itens[i].getAttribute('data-locale')];
+
+    if (itemLocale) {
+      itens[i].innerHTML = itemLocale;
+    }
+  }
+}
